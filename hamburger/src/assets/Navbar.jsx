a120942ca0a88bf7a08633149b3476c44637426e
jamalshamsi <111067644+shararesadeghi@users.noreply.github.com>
2023-07-27 16:03:32 +0430

    project completed

--- hamburger/src/assets/Navbar.jsx
+++ hamburger/src/assets/Navbar.jsx
@@ -1,11 +1,36 @@
 import React from 'react';
 import styled from "styled-components";
 
-const Ul = styled.ul``;
+const Ul = styled.ul`
+list-style:none;
+display:flex;
+justify-content:space-between;
+margin:20px 50px;
+z-index:15;
+li{
+    padding:15px 30px;
+}
+@media (max-width:768px){
+    margin:0;
+    flex-direction:column;
+    background:blue;
+    position:fixed;
+   transition : all 0.3s linear;
+   transform : ${props => props.open ? "translateX(0)" : "translateX(100%)"};
+    top:0;
+    right:0;
+    height:100vh;
+    width:300px;
+    padding-top:3.5rem;
+    li{
+        color:#fff;
+    }
+}
+`;
 
-const Navbar = () => {
+const Navbar = ({open}) => {
     return (
-        <Ul>
+        <Ul open={open}>
             <li>Home</li>
             <li>Dos</li>
             <li>Buy me a coffee</li>
